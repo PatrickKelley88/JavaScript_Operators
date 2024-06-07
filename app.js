5898ac677dc937694aa1ea468786e028cb232e20
Patrick Kelley <PatrickO.kelley88@gmail.com>
2024-06-07 09:38:55 -0500

    completed Java Operators exercise

--- app.js
+++ app.js
@@ -5,15 +5,67 @@ console.log(
 
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a *b;
+let dividing = a / b;
+console.log(add, minus, multiply, dividing);
 
 // YOUR CODE HERE
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
 
-// YOUR CODE HERE
+num + str
+console.log(num + str); //1111
+
+num + str2
+console.log(num + str2); //11eleven
+
+num = isPresent
+console.log(num + isPresent); //12
+
+firstName + num
+console.log(firstName + num); //Frodo11
+
+isPresent + str
+console.log(isPresent + str); //true11
+
+firstName + lastName
+console.log(firstName + lastName);//FrodoBaggins
+
+
+
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
 
-// YOUR CODE HERE
+console.log(val == str3)//true
+
+console.log(val === str3)//false
+
+console.log(!isPresent2)//true
+
+console.log("eleven" == str4 && val >= str3); // false
+
+console.log(!isPresent2 || isPresent2)//true
+ 
+console.log(0 == false);//true
+ 
+console.log(0 === false);//false
+
+console.log(0 != false);//false
+
+console.log(0 !== false);//true
